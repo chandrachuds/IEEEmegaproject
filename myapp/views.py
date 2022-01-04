35db3c0460490e9b2fa14524b574be62ed9b146d
chandrachuds <chandrachudshrivastava@gmail.com>
2022-01-04 13:20:56 +0530

    Sign Up

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -17,15 +19,54 @@ from random import randint
 def index(request):
     return render(request,'index.html')
 
-def sign_up(request):
-    return render(request,'index.html')
 # def counter(request):
 #     text = request.POST['text']
 #     amount_of_words = len(text.split())
 #     return render(request,'counter.html',{'amount': amount_of_words})
 def register(request):
-  return render(request,'register.html')
+  if request.method == 'POST':
+      username = request.POST['username']
+      email = request.POST['email']
+      password = request.POST['password']
+      password2 = request.POST['password2']
 
+      if password == password2:
+        if User.objects.filter(email=email).exists():
+          messages.info(request,'Email Already Used')
+          return redirect('register')
+        elif User.objects.filter(username=username).exists():
+          messages.info(request,'Username Already Used')
+          return redirect('register')
+        else:
+          user = User.objects.create_user(username=username,email=email,password=password2)
+          user.save()
+          return redirect('login')
+      else:
+        messages.info(request,'Passwords Are Different')
+        return redirect('register')
+  else:
+      return render(request,'register.html')
+
+def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username,password=password)
+
+    if user is not None:
+      auth.login(request,user)
+      return redirect('index')
+    else:
+      messages.info(request,'Invalid Credentials')
+      return redirect('login')
+  else:  
+    return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+  
 def counter(request):
     #Taking Input form Webpage
     text = request.POST['text']
